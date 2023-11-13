928f9e1a556c7645012b68ef3fd256542c52ef46
Eric Danker <eric@breomedia.com>
2023-11-12 22:03:32 -0600

    Hacky Pandas Interface Working

--- gsheet_temp.py
+++ gsheet_temp.py
@@ -1,4 +1,5 @@
 import pygsheets # https://pygsheets.readthedocs.io/en/stable/index.html
+import pandas as pd
 
 client = pygsheets.authorize() # https://pygsheets.readthedocs.io/en/stable/authorization.html
 
@@ -6,12 +7,24 @@ client = pygsheets.authorize() # https://pygsheets.readthedocs.io/en/stable/auth
 sh = client.open('Tech Jobs - Database')
 wks = sh.sheet1
 
-# Update a single cell.
-wks.update_value('A1', "FIRST")
+#Get the data from the Sheet into python as DF
+jobs_df = wks.get_as_df()
 
-wks.append_table([1, 2, 3], dimension='ROWS')
-wks.append_table([2, 2, 3], dimension='ROWS')
-wks.append_table([3, 2, 3], dimension='ROWS')
+#Print the head of the datframe
+print(jobs_df)
+
+#In order to clear all the data in the sheet.
+wks.clear()
+
+jobs_df = pd.concat([jobs_df, pd.DataFrame.from_records(
+    [{ 'Job Title': 'X1', 'Job Location': 'XJL',  'Job URL': 'XJU'}])])
+
+#We will use set_dataframe() in pygsheets to write df data into GS
+wks.set_dataframe(jobs_df,(1,1))    #(Row_Number, Column_Number)
+
+# Write a few rows
+#wks.append_table(data, dimension='ROWS', overwrite=True)
+
 
 # Update the worksheet with the numpy array values. Beginning at cell 'A2'.
 #wks.update_values('A2', my_numpy_array.to_list())
